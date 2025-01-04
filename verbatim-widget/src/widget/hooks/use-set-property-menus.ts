63445f97e138a96f22625463e647abd5e0ed9877
ahnopologetic <stahn1995@gmail.com>
2025-01-04 08:44:49 -0500

    feat: add home icon

--- verbatim-widget/src/widget/hooks/use-set-property-menus.ts
+++ verbatim-widget/src/widget/hooks/use-set-property-menus.ts
@@ -1,5 +1,12 @@
 import { usePropertyMenu } from "../lib";
 
+const HOME_SVG_SRC = `
+<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="800px" height="800px" viewBox="0 0 16 16" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M8 0L0 6V8H1V15H4V10H7V15H15V8H16V6L14 4.5V1H11V2.25L8 0ZM9 10H12V13H9V10Z" fill="#FFFFFF"/>
+</svg>
+`
+
 export const useSetPropertyMenu = (widgetType: string, mainWidgetId: string) => {
     const focusOnMainWidget = () => {
         const mainWidget = figma.getNodeById(mainWidgetId) as WidgetNode;
@@ -13,8 +20,8 @@ export const useSetPropertyMenu = (widgetType: string, mainWidgetId: string) => 
         {
             itemType: 'action',
             propertyName: 'home',
-            tooltip: 'Go to home',
-            icon: 'home'
+            tooltip: 'Home',
+            icon: HOME_SVG_SRC
         },
         {
             itemType: 'separator',
@@ -35,7 +42,7 @@ export const useSetPropertyMenu = (widgetType: string, mainWidgetId: string) => 
 
     usePropertyMenu(
         getPropertyMenu(widgetType),
-        ({propertyName}) => {
+        ({ propertyName }) => {
             if (widgetType === 'event') {
                 switch (propertyName) {
                     case 'home':
